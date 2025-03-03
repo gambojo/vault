b2eb05a8115cd626b5b15685b842884a8694f159
Agamov Gamid <agamovga@cloudx.group>
2025-03-04 00:55:13 +0300

    Vault

--- molecule/single/tests/test_single.py
+++
@@ -1,8 +0,0 @@
-def test_service_is_running(host):
-    service = host.service("vault")
-    assert service.is_running
-    assert service.is_enabled
-
-def test_port_is_listning(host):
-    socket = host.socket('tcp://0.0.0.0:8200')
-    assert socket.is_listening
